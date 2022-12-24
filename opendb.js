79cac389b54a0e8e6bb98255f913b65205b26bac
pankaj bisht <pankajbisht031@gmail.com>
2022-12-24 20:40:05 +0530

    js indentation

--- opendb.js
+++ opendb.js
@@ -6,246 +6,243 @@
 (function () {
     "use strict";
 
+    var Operation = function (storage) {
+        var get, set, has, remove, clean, parse, string, setJSON, getJSON,
+        trim, count, trim, key, setMore, removeMore, getMore, counter, resetCounter,
+        keys, getVLStartWith, getOBStartWith, entire;
 
-var Operation = function (storage) {
-    var get, set, has, remove, clean, parse, string, setJSON, getJSON,
-    trim, count, trim, key, setMore, removeMore, getMore, counter, resetCounter,
-    keys, getVLStartWith, getOBStartWith, entire;
-
-    get = function (key) {
-        return storage.getItem(key);
-    };
-
-    set = function (key, value) {
-        storage.setItem(key, value);
-    };
-
-    has = function (key) {
-        return !!storage.getItem(key);
-    };
-
-    remove = function (key) {
-        return storage.removeItem(key);
-    };
-
-    clean = function () {
-        storage.clear();
-    };
-
-    parse = function (obj) {
-        return JSON.parse(obj);
-    };
-
-    string = function (obj) {
-        return JSON.stringify(obj);
-    };
-
-    setJSON = function (key, obj) {
-        this.set(key, this.string(obj));
-    };
-
-    getJSON = function (key) {
-        return this.parse(this.get(key));
-    };
-
-    trim = function (key) {
-        return this.get(key).replace(/(^\s*)|(\s*$)/g, "");;
-    };
-
-    count = function () {
-        return storage.length;
-    };
-
-    key = function (i) {
-        return storage.key(i);
-    };
-
-    setMore = function (obj) {
-        var len = Object.keys(obj).length, i = 0, keys = Object.keys(obj),
-                child = MyError;
-
-        child.isValid(obj, "object");
-
-        while (len--) {
-            this.set(keys[i], obj[keys[i]]);
-            i++;
-        }
-
-        return true;
-    };
-
-    removeMore = function (coll) {
-        var len = coll.length, i = 0, arr = [],
-            child = MyError;
-
-
-        child.isValid(coll, "array");
-
-        while(len--) {
-            this.remove(coll[i]);
-            i++;
-        }
-
-        return true;
-    };
-
-    getMore = function (coll) {
-        var len = coll.length, i = 0, arr = [],
-            child = MyError;
-
-        child.isValid(coll, "array");
-
-        while(len--) {
-            arr.push(this.get(coll[i]));
-            i++;
-        }
-
-        return arr;
-    };
-
-    counter = function () {
-        var get = parseInt(this.get("counter"));
-        var val = get ? get : 0;
-
-        this.set("counter", ++val);
-        return parseInt(this.get("counter"));
-    };
-
-    resetCounter = function () {
-        this.remove("counter");
-    };
-
-    keys = function () {
-        var len = storage.length, arr = [];
-
-        for (var i = 0; i < len; i++) {
-            arr.push(storage.key(i));
+        get = function (key) {
+            return storage.getItem(key);
         };
 
-        return arr;
-    };
+        set = function (key, value) {
+            storage.setItem(key, value);
+        };
 
-    getVLStartWith = function (str, start, end) {
-        var len = this.count(), i = 0, arr = [];
+        has = function (key) {
+            return !!storage.getItem(key);
+        };
 
-        while (len--) {
+        remove = function (key) {
+            return storage.removeItem(key);
+        };
 
-            var key = this.key(i); //here
+        clean = function () {
+            storage.clear();
+        };
 
-            if (key.substring(start, end) === str) {
-                arr.push(this.get(key)); //here
+        parse = function (obj) {
+            return JSON.parse(obj);
+        };
+
+        string = function (obj) {
+            return JSON.stringify(obj);
+        };
+
+        setJSON = function (key, obj) {
+            this.set(key, this.string(obj));
+        };
+
+        getJSON = function (key) {
+            return this.parse(this.get(key));
+        };
+
+        trim = function (key) {
+            return this.get(key).replace(/(^\s*)|(\s*$)/g, "");;
+        };
+
+        count = function () {
+            return storage.length;
+        };
+
+        key = function (i) {
+            return storage.key(i);
+        };
+
+        setMore = function (obj) {
+            var len = Object.keys(obj).length, i = 0, keys = Object.keys(obj),
+                    child = MyError;
+
+            child.isValid(obj, "object");
+
+            while (len--) {
+                this.set(keys[i], obj[keys[i]]);
+                i++;
             }
 
-            i++;
-        }
+            return true;
+        };
 
-        return arr;
-    };
+        removeMore = function (coll) {
+            var len = coll.length, i = 0, arr = [],
+                child = MyError;
 
-    getOBStartWith = function (str, start, end) {
-        var len = this.count(), i = 0, obj = {};
 
-        while (len--) {
+            child.isValid(coll, "array");
 
-            var key = this.key(i); //here
+            while(len--) {
+                this.remove(coll[i]);
+                i++;
+            }
 
-            if (key.substring(start, end) === str) {
+            return true;
+        };
+
+        getMore = function (coll) {
+            var len = coll.length, i = 0, arr = [],
+                child = MyError;
+
+            child.isValid(coll, "array");
+
+            while(len--) {
+                arr.push(this.get(coll[i]));
+                i++;
+            }
+
+            return arr;
+        };
+
+        counter = function () {
+            var get = parseInt(this.get("counter"));
+            var val = get ? get : 0;
+
+            this.set("counter", ++val);
+            return parseInt(this.get("counter"));
+        };
+
+        resetCounter = function () {
+            this.remove("counter");
+        };
+
+        keys = function () {
+            var len = storage.length, arr = [];
+
+            for (var i = 0; i < len; i++) {
+                arr.push(storage.key(i));
+            };
+
+            return arr;
+        };
+
+        getVLStartWith = function (str, start, end) {
+            var len = this.count(), i = 0, arr = [];
+
+            while (len--) {
+
+                var key = this.key(i); //here
+
+                if (key.substring(start, end) === str) {
+                    arr.push(this.get(key)); //here
+                }
+
+                i++;
+            }
+
+            return arr;
+        };
+
+        getOBStartWith = function (str, start, end) {
+            var len = this.count(), i = 0, obj = {};
+
+            while (len--) {
+
+                var key = this.key(i); //here
+
+                if (key.substring(start, end) === str) {
+                    obj[key] = key;
+                    obj[key] = this.get(key); //here
+                }
+
+                i++;
+            }
+
+            return obj;
+        };
+
+        entire = function (str, start, end) {
+            var len = this.count(), i = 0, obj = {};
+
+            while (len--) {
+
+                var key = this.key(i); //here
+
                 obj[key] = key;
                 obj[key] = this.get(key); //here
+                i++;
             }
 
-            i++;
-        }
+            return obj;
+        };
 
-        return obj;
+        return {
+            get: get,
+            set: set,
+            has: has,
+            remove: remove,
+            clean: clean,
+            parse: parse,
+            string: string,
+            setJSON: setJSON,
+            getJSON: getJSON,
+            count: count,
+            trim: trim,
+            key: key,
+            setMore: setMore,
+            removeMore: removeMore,
+            getMore: getMore,
+            counter: counter,
+            resetCounter: resetCounter,
+            keys: keys,
+            getVLStartWith: getVLStartWith,
+            getOBStartWith: getOBStartWith,
+            entire: entire
+        };
     };
 
-    entire = function (str, start, end) {
-        var len = this.count(), i = 0, obj = {};
+    var Cookie = (function () {
+        var set, get, has, entire;
 
-        while (len--) {
+        set = function (key, value, exdays) {
+            var d = new Date();
+            d.setTime(d.getTime() + (exdays*24*60*60*1000));
+            var expires = "expires=" + d.toGMTString();
+            document.cookie = key + "=" + value +"; " + expires;
+        };
 
-            var key = this.key(i); //here
+        get = function (key) {
+            var name = key + "=";
+            var ca = document.cookie.split(';');
 
-            obj[key] = key;
-            obj[key] = this.get(key); //here
-            i++;
-        }
+            for(var i = 0; i < ca.length; i++) {
+                var c = ca[i];
 
-        return obj;
-    };
+                while (c.charAt(0) === ' ') c = c.substring(1);
 
-    return {
-        get: get,
-        set: set,
-        has: has,
-        remove: remove,
-        clean: clean,
-        parse: parse,
-        string: string,
-        setJSON: setJSON,
-        getJSON: getJSON,
-        count: count,
-        trim: trim,
-        key: key,
-        setMore: setMore,
-        removeMore: removeMore,
-        getMore: getMore,
-        counter: counter,
-        resetCounter: resetCounter,
-        keys: keys,
-        getVLStartWith: getVLStartWith,
-        getOBStartWith: getOBStartWith,
-        entire: entire
-    };
-};
-
-
- var Cookie = (function () {
-    var set, get, has, entire;
-
-    set = function (key, value, exdays) {
-        var d = new Date();
-        d.setTime(d.getTime() + (exdays*24*60*60*1000));
-        var expires = "expires=" + d.toGMTString();
-        document.cookie = key + "=" + value +"; " + expires;
-    };
-
-    get = function (key) {
-        var name = key + "=";
-        var ca = document.cookie.split(';');
-
-        for(var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-
-            while (c.charAt(0) === ' ') c = c.substring(1);
-
-            if (c.indexOf(name) ===  0) {
-                return c.substring(name.length, c.length);
+                if (c.indexOf(name) ===  0) {
+                    return c.substring(name.length, c.length);
+                }
             }
-        }
 
-        return "";
-    };
+            return "";
+        };
 
-    has = function (key) {
-        return !!this.get(key);
-    };
+        has = function (key) {
+            return !!this.get(key);
+        };
 
-    entire = function () {
-        return document.cookie;
-    };
+        entire = function () {
+            return document.cookie;
+        };
 
-    return {
-        get: get,
-        set: set,
-        has: has,
-        entire: entire
-    };
-}());
+        return {
+            get: get,
+            set: set,
+            has: has,
+            entire: entire
+        };
+    }());
 
-
-var MyError = (function () {
+    var MyError = (function () {
     var type, isValid;
 
     type = (function (global) {
@@ -281,8 +278,7 @@ var MyError = (function () {
     };
 }());
 
-
-var Testing = (function () {
+    var Testing = (function () {
     var log, assert;
 
     log = function (msg) {
@@ -299,7 +295,7 @@ var Testing = (function () {
     };
 }());
 
-var WEBSQL = (function () {
+    var WEBSQL = (function () {
     var initDB,
         createTable,
         insertRow,
@@ -359,7 +355,7 @@ var WEBSQL = (function () {
     }
 }());
 
- var Main = (function () {
+    var Main = (function () {
 
     var superC = {}, local = localStorage, session = sessionStorage;
 
@@ -381,8 +377,6 @@ var WEBSQL = (function () {
 
     return superC;
 }());
-
-
 
     window.db = Main;
 }());
