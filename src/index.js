59cddd4f22546a8c9d108a1b76678d18fd174e5f
pankaj bisht <pankajbisht031@gmail.com>
2023-11-26 22:40:26 +0530

    webpack-html-plugin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import * as db from "./module/index";
 
+var Main = (function () {
+    var odb = {}, local = localStorage, session = sessionStorage;
+    odb.local = Operations(local);
+    odb.session = Operations(session);
+
+    return odb;
+})
+
 export default db;
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,30 @@
 const path = require("path");
+const htmlwebpackplugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: {
+        index: "./src/bundle.js",
+        todo: "./src/bundle.js"
+    },
     output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist")
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
     },
     devServer: {
         static: "./dist",
-    }
+    },
+    plugins: [
+        new htmlwebpackplugin({
+            template: path.resolve(__dirname, "ex/index.html"),
+            chunks: ["index"],
+            inject: true,
+            filename: "index.html"
+        }),
+        new htmlwebpackplugin({
+            template: path.resolve(__dirname, "ex/todo.html"),
+            chunks: ["todo"],
+            inject: true,
+            filename: "todo.html"
+        })
+    ]
 }
